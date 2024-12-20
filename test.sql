5ddd3cf224b4e4d7001af0f6f8e7173c397dfd30
Dewa Assault <you@example.com>
2024-12-20 19:59:49 +0700

    change db

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2024 at 01:56 PM
+-- Generation Time: Dec 20, 2024 at 01:58 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -48,6 +48,35 @@ INSERT INTO `mahasiswa` (`NIM`, `Nama`, `Alamat`, `Jurusan`, `Umur`) VALUES
 ('678901', 'Emily', 'Jl. Cendrawasih No. 4', 'Manajemen', 24),
 ('789012', 'Frank', 'Jl. Ahmad Yani No. 6', 'Teknik Informatika', 19);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mata_kuliah`
+--
+
+CREATE TABLE `mata_kuliah` (
+  `ID` int(11) NOT NULL,
+  `Mata_Kuliah` varchar(50) DEFAULT NULL,
+  `NIM` varchar(10) DEFAULT NULL,
+  `Nilai` int(11) DEFAULT NULL,
+  `Dosen_Pengajar` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `mata_kuliah`
+--
+
+INSERT INTO `mata_kuliah` (`ID`, `Mata_Kuliah`, `NIM`, `Nilai`, `Dosen_Pengajar`) VALUES
+(1, 'Pemrograman Web', '123456', 85, 'Pak Budi'),
+(2, 'Basis Data', '234567', 70, 'Ibu Ani'),
+(3, 'Jaringan Komputer', '345678', 75, 'Pak Dodi'),
+(4, 'Sistem Operasi', '123456', 90, 'Pak Budi'),
+(5, 'Manajemen Proyek', '456789', 80, 'Ibu Desi'),
+(6, 'Bahasa Inggris', '567890', 85, 'Ibu Eka'),
+(7, 'Statistika', '678901', 75, 'Pak Farhan'),
+(8, 'Algoritma', '789012', 65, 'Pak Galih'),
+(9, 'Pemrograman Java', '123456', 95, 'Pak Budi');
+
 --
 -- Indexes for dumped tables
 --
@@ -57,6 +86,23 @@ INSERT INTO `mahasiswa` (`NIM`, `Nama`, `Alamat`, `Jurusan`, `Umur`) VALUES
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`NIM`);
+
+--
+-- Indexes for table `mata_kuliah`
+--
+ALTER TABLE `mata_kuliah`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `NIM` (`NIM`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `mata_kuliah`
+--
+ALTER TABLE `mata_kuliah`
+  ADD CONSTRAINT `mata_kuliah_ibfk_1` FOREIGN KEY (`NIM`) REFERENCES `mahasiswa` (`NIM`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
